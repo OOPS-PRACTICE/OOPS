68c1a1f6872de7e6dffeeeb0138564e804e8a3b6
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-08-07 10:32:13 +0530

    Update Constructor.java

--- Constructor/Constructor.java
+++ Constructor/Constructor.java
@@ -1,1 +1,20 @@
+package Constructor;
+
+public class Constructor {
+
+    public static void main(String[] args){
+        Car car1 = new Car();
+        car1.isAutomaticCar = false;
+        car1.mileage = 100;
+
+        Car car2 = new Car();
+        car2.isAutomaticCar = true;
+        car1.mileage = 150;
+        //car1.ownerName("Umesh Batra");
+        /*System.out.println(car.isAutomaticCar);
+        System.out.println(car.model);
+        System.out.println(car.NATIONALITY);
+        System.out.println(car.mileage);*/
+    }
+}
 
