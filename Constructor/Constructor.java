01a17ae6f1a8f1938cfcce34ff6a2bfde8f3ba5f
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-08-09 10:19:09 +0530

    Update Constructor.java

--- Constructor/Constructor.java
+++ Constructor/Constructor.java
@@ -3,18 +3,18 @@ package OOPS.Constructor;
 public class Constructor {
 
     public static void main(String[] args){
-        Car car1 = new Car();
-        car1.isAutomaticCar = false;
-        car1.mileage = 100;
+        
+        Car car1 = new Car(15, "Amaze", "ownerName1", false);
+        System.out.println(car1.getMileage());
+        System.out.println(car1.getModel());
+        System.out.println(car1.getOwnerName());
+        System.out.println(car1.isAutomaticCar);
 
-        Car car2 = new Car();
-        car2.isAutomaticCar = true;
-        car1.mileage = 150;
-        //car1.ownerName("Owner Name");
-        /*System.out.println(car.isAutomaticCar);
-        System.out.println(car.model);
-        System.out.println(car.NATIONALITY);
-        System.out.println(car.mileage);*/
+        Car car2 = new Car(12, "Mercedes", "ownerName2", true);
+        System.out.println(car2.getMileage());
+        System.out.println(car2.getModel());
+        System.out.println(car2.getOwnerName());
+        System.out.println(car2.isAutomaticCar);
     }
 }
 
