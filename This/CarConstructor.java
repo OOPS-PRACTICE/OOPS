ebfc2e96e5786a73a39d32795d10210d6855c8e8
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-08-09 10:40:10 +0530

    Update CarConstructor.java

--- This/CarConstructor.java
+++ This/CarConstructor.java
@@ -1,1 +1,20 @@
+package OOPS.This;
 
+public class CarConstructor {
+
+    public static void main(String[] args) {
+
+        Car car1 = new Car(15, "Amaze", "ownerName1", false);
+        System.out.println(car1.getMileage());
+        System.out.println(car1.getModel());
+        System.out.println(car1.getOwnerName());
+        System.out.println(car1.isAutomaticCar);
+
+        Car car2 = new Car(12, "Mercedes", "ownerName2", true);
+        System.out.println(car2.getMileage());
+        System.out.println(car2.getModel());
+        System.out.println(car2.getOwnerName());
+        System.out.println(car2.isAutomaticCar);
+
+    }
+}
