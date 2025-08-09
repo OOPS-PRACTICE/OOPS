daa412a1acc84bee31bf8c00e18718d32d5cb0e3
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-08-09 10:37:48 +0530

    Update Car.java

--- This/Car.java
+++ This/Car.java
@@ -1,1 +1,36 @@
+package OOPS.This;
 
+public class Car {
+
+    public int mileage;
+    public String model;
+    public String ownerName;
+    public boolean isAutomaticCar;
+
+    public Car(int mileage, String model, String ownerName, boolean isAutomaticCar){
+        this.mileage = mileage;
+        this.model = model;
+        this.ownerName = ownerName;
+        this.isAutomaticCar = isAutomaticCar;
+    }
+
+    public Car(){
+
+    }
+
+    public int getMileage() {
+        return this.mileage;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public String getOwnerName() {
+        return this.ownerName;
+    }
+
+    public boolean isAutomaticCar() {
+        return this.isAutomaticCar;
+    }
+}
