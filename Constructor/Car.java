f9107c3df36f197b834034a98259a614467c5070
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-08-09 10:14:43 +0530

    Update Car.java

--- Constructor/Car.java
+++ Constructor/Car.java
@@ -2,23 +2,35 @@ package OOPS.Constructor;
 
 public class Car {
 
-    static int count = 0;
     public int mileage;
     public String model;
     public String ownerName;
-    public String number;
     public boolean isAutomaticCar;
 
-    public String NATIONALITY = "INDIAN";
-
-    public Car(int mileage, String model, String ownerName, String number, boolean isAutomaticCar){
-        count++;
-        System.out.println("I am in constructor " + count);
-
+    public Car(int mileage1, String model1, String ownerName1, boolean isAutomaticCar1){
+        mileage = mileage1;
+        model = model1;
+        ownerName = ownerName1;
+        isAutomaticCar = isAutomaticCar1;
     }
 
     public Car(){
 
     }
 
+    public int getMileage() {
+        return mileage;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public boolean isAutomaticCar() {
+        return isAutomaticCar;
+    }
 }
