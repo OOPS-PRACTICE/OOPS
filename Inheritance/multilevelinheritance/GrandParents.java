0691943e3945a2c63e4e2d5f10e7ac992d70e16c
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-08-19 08:47:33 +0530

    Create GrandParents.java

---
+++ Inheritance/multilevelinheritance/GrandParents.java
@@ -0,0 +1,26 @@
+public class GrandParents {
+
+    int age;
+    String name;
+
+
+    public void grandParentMethod(){
+        System.out.println("In grand parent method");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
