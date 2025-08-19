75ac0e72e93fdcae9744f01101b97647c6f7eb6f
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-08-19 08:42:59 +0530

    Create HierarchialInheritance.java

---
+++ Inheritance/HierarchialInheritance.java
@@ -0,0 +1,15 @@
+public class HierarchialInheritance {
+
+    public static void main(String[] args){
+        Cat cat = new Cat();
+        cat.walk();
+        cat.speak();
+
+        Dog dog = new Dog();
+        dog.walk();
+        dog.speak();
+
+        //Super Example
+        //Override example
+}
+}
