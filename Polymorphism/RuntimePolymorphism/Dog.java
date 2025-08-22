dddcea5a6b185c8b0cc92dcbf77eb99461387406
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-08-22 15:06:22 +0530

    Create Dog.java

---
+++ Polymorphism/RuntimePolymorphism/Dog.java
@@ -0,0 +1,10 @@
+public class Dog extends Animal {
+
+    public void speak(){
+        System.out.println("bow-wow");
+    }
+
+    public void likeWalking(){
+        System.out.println("Details");
+    }
+}
