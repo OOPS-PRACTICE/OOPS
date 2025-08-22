39f3f45138d6f0eec28babf92745429834159a8d
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-08-22 15:01:46 +0530

    Create SingleInheritance.java

---
+++ Inheritance/SingleInheritance/SingleInheritance.java
@@ -0,0 +1,16 @@
+public class SingleInheritance {
+
+    public static void main(String[] args) {
+
+        //MountainBike mountainBike = new MountainBike();
+
+
+        MountainBike mountainBike = new MountainBike(1,2,3,4);
+
+        //mountainBike.checkBicycleMethod();
+
+        //super
+        //this : memory example
+    }
+
+}
