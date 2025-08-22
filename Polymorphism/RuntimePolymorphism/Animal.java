006552de4fdda187e67aa4b21a098bc6acfdefc5
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-08-22 15:05:10 +0530

    Create Animal.java

---
+++ Polymorphism/RuntimePolymorphism/Animal.java
@@ -0,0 +1,14 @@
+public class Animal {
+
+    int age;
+    int name;
+
+    public void walk(){
+        System.out.println("Animal is walking");
+    }
+
+    public void speak(){
+        System.out.println(".......");
+    }
+
+}
