6eab9b8c1d0f99874430cd3a5822217ef6af6c7f
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-08-22 14:52:48 +0530

    Create Dog.java

---
+++ Inheritance/HierarchialInheritance/Dog.java
@@ -0,0 +1,14 @@
+public class Dog extends Animal{
+
+    public void speak(){
+        System.out.println("bow-wow");
+    }
+
+    Dog(){
+
+    }
+    Dog(int a, int b){
+       //super(a,b);
+       super();
+    }
+}
