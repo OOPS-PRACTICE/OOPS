d9a8a1c41cf84e958f484372e5e6032badb6f6ac
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-08-22 14:58:37 +0530

    Create ObjectSingleInheritance.java

---
+++ Inheritance/SingleInheritance/ObjectSingleInheritance.java
@@ -0,0 +1,7 @@
+public class ObjectSingleInheritance extends Object {
+
+    public static void main(String[] args) {
+        Object obj = new Object();
+        System.out.println(obj.hashCode());
+    }
+}
