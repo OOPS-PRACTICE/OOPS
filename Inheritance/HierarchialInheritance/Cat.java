7a7344b7bf70f5ce1ff600160ae99ce4fe0d2cf8
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-08-22 14:52:12 +0530

    Create Cat.java

---
+++ Inheritance/HierarchialInheritance/Cat.java
@@ -0,0 +1,15 @@
+public class Cat extends Animal{
+
+    public void speak(){
+        System.out.println("meow");
+    }
+
+    public void speak1(){
+        System.out.println("meow");
+    }
+/*
+    public void speak(int a){
+
+    }*/
+
+}
