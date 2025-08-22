797b8bc51587fff31ad06e7d3cd3c2b284a7c094
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-08-22 15:00:20 +0530

    Create Bicycle.java

---
+++ Inheritance/SingleInheritance/Bicycle.java
@@ -0,0 +1,55 @@
+public class Bicycle {
+
+    public int cadence;
+    public int gear;
+    public int speed;
+
+
+    public Bicycle(){
+        System.out.println("In a Bicycle class default constructor");
+    }
+
+    public void checkBicycleMethod(){
+
+        System.out.println("In a Bicycle Check method");
+
+        /*System.out.println("Step 1 : Check Handle");
+        System.out.println("Step 2 : Check Gear");
+        System.out.println("Step 3 : Apply Oiling");
+        System.out.println("Step 4 : Apply Grease");
+*/
+
+    }
+
+
+
+
+
+
+
+
+    // the Bicycle class has one constructor
+    /*public Bicycle(int startCadence, int startSpeed, int startGear) {
+        gear = startGear;
+        cadence = startCadence;
+        speed = startSpeed;
+    }*/
+
+    // the Bicycle class has four methods
+    /*public void setCadence(int newValue) {
+        cadence = newValue;
+    }
+
+    public void setGear(int newValue) {
+        gear = newValue;
+    }
+
+    public void applyBrake(int decrement) {
+        speed -= decrement;
+    }
+
+    public void speedUp(int increment) {
+        speed += increment;
+    }*/
+
+}
