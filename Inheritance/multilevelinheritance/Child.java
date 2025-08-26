ff2c26e05a2eb1efa6697c6c67be4c9fe76018e3
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-08-26 10:59:51 +0530

    Update Child.java

--- Inheritance/multilevelinheritance/Child.java
+++ Inheritance/multilevelinheritance/Child.java
@@ -4,4 +4,8 @@ public class Child extends Parents {
     public void childMethod(){
         System.out.println("In child method");
     }
+
+    public void test(){
+        System.out.println("In child class");
+    }
 }
