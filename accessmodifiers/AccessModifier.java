92fe51fe447a2f9bd26415b6cf502708447b765b
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-08-26 10:56:01 +0530

    Create AccessModifier.java

---
+++ accessmodifiers/AccessModifier.java
@@ -0,0 +1,18 @@
+public class AccessModifier {
+
+    public void test(){
+
+    }
+
+
+    public AccessModifier(){
+
+    }
+
+    protected void testMethod()
+    {
+        System.out.println("Test Access Modifier");
+    }
+
+
+}
