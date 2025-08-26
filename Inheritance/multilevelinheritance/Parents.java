26a00ad4591f1ff8bb4c93cb4a706967f9f373b9
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-08-26 10:59:18 +0530

    Update Parents.java

--- Inheritance/multilevelinheritance/Parents.java
+++ Inheritance/multilevelinheritance/Parents.java
@@ -4,4 +4,8 @@ public class Parents extends GrandParents {
     public void parentMethod(){
         System.out.println("In parent method");
     }
+
+     public void test(){
+       System.out.println("In a Parent class");
+    }
 }
