3afe1ea30bcd77b422870e95fcc2644259f47df3
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-08-26 10:58:12 +0530

    Create TestAccessModifier.java

---
+++ Constructor/TestAccessModifier.java
@@ -0,0 +1,11 @@
+public class TestAccessModifier extends AccessModifier {
+    public static void main(String args[])
+    {
+        /*TestAccessModifier obj = new TestAccessModifier();
+        obj.testMethod();*/
+
+        System.out.println(Integer.MAX_VALUE);
+        AccessModifier am = new AccessModifier();
+        am.test();
+    }
+}
