29f520cb8b99d1c4c8b49b7dedf6ccf84daab8a8
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-08-27 10:38:01 +0530

    Create Main.java

---
+++ Static/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    static int abc = 10;
+
+    public static void main(String args[]){
+
+        Bicycle.getCount();
+
+
+      /*  Main m = new Main();
+        //m.test();
+
+        test();
+
+
+        int xyz = abc;*/
+
+        Bicycle bicycle1 = new Bicycle();
+        System.out.println(bicycle1.getCount());
+
+
+      /*  Bicycle bicycle2 = new Bicycle();
+
+        System.out.println(bicycle2.getCount());
+*/
+    }
+
+    public static void test(){
+
+    }
+
+}
