ead9bf2f4bd38bf3120dd405ba8050d3f82a96ba
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-08-29 12:35:55 +0530

    Create Dog.java

---
+++ Abstraction/Dog.java
@@ -0,0 +1,13 @@
+
+public class Dog extends Animal {
+
+    @Override
+   protected void speak() {
+   System.out.println("BOW BOW");
+    }
+
+    @Override
+    protected void laugh() {
+
+    }
+}
