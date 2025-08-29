42d865154e642e0b3a77768184f7d48a84e415f2
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-08-29 12:38:39 +0530

    Create Rectangle.java

---
+++ Abstraction/Rectangle.java
@@ -0,0 +1,12 @@
+
+public class Rectangle extends GraphicObject {
+    @Override
+    void draw() {
+        System.out.println("Drawing in rectangle class");
+    }
+
+    @Override
+    void resize() {
+        System.out.println("Resize in rectangle class");
+    }
+}
