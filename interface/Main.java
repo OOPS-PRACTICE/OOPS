2e66215ec49603ce1dbbcbe475aab5ee755f628f
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-09-06 21:00:00 +0530

    Create Main.java

---
+++ interface/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args){
+        Vehicle car = new Car();
+        car.test();
+        Vehicle car1 = new Car();
+        // car1.test();
+    }
+}
