748f88a95b3a7235e635a69f3aff7be298aa7090
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-09-10 22:14:51 +0530

    Create Test.java

---
+++ innerclasses/local/Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        //Validation Logic Encapsulated
+        Hotel hotel = new Hotel("Test", 10, 5);
+        hotel.reserveRoom("Name", 10);
+    }
+}
