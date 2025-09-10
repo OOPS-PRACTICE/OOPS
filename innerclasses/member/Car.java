cd72c3b0b3de222b86352f324eb5e27275fab919
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-09-10 22:06:07 +0530

    Create Car.java

---
+++ innerclasses/member/Car.java
@@ -0,0 +1,31 @@
+
+public class Car {
+
+    private String model;
+    private boolean isEngineOn;
+
+    public Car(String model){
+        this.model = model;
+        this.isEngineOn = false;
+    }
+
+    class Engine {
+        void start(){
+            if(!isEngineOn){
+                isEngineOn = true;
+                System.out.println(model + "Engine is started");
+            }else{
+                System.out.println(model + "Engine is already started");
+            }
+        }
+
+        void stop(){
+            if(isEngineOn){
+                isEngineOn = false;
+                System.out.println(model + "Engine is stopped now");
+            }else{
+                System.out.println(model + "Engine is already off");
+            }
+        }
+    }
+}
