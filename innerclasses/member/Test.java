ca71861a1f196df4bcf5954aeb282cacd97495ec
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-09-10 22:06:48 +0530

    Create Test.java

---
+++ innerclasses/member/Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+
+    public static void main(String[] args) {
+        Car car = new Car("Tesla");
+        Car.Engine engine = car.new Engine();
+        engine.start();
+        engine.stop();
+    }
+}
