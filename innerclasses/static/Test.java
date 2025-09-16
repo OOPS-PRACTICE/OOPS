c692640bc5a6d61b2803ce2834dcbcee6d398693
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-09-16 10:55:43 +0530

    Create Test.java

---
+++ innerclasses/static/Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+    public static void main(String[] args) {
+        Computer comp = new Computer("HP","ABC","Windows");
+        comp.getOs().displayName();
+
+        Computer.USB usb = new Computer.USB("Type C");
+
+
+    }
+}
