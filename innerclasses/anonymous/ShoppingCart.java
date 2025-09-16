f9665dd3342158d197f96a792cf70114ac097cb4
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-09-16 10:57:02 +0530

    Create ShoppingCart.java

---
+++ innerclasses/anonymous/ShoppingCart.java
@@ -0,0 +1,14 @@
+
+
+public class ShoppingCart {
+
+   private double totalAmount;
+
+    public ShoppingCart(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public void processPayment(Payment payment){
+        payment.pay(totalAmount);
+    }
+}
