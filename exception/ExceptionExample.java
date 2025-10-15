e5b4826716e43c2ba6c4216f45eb65f8a55b1f5c
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-10-15 10:54:47 +0530

    Update ExceptionExample.java

--- exception/ExceptionExample.java
+++ exception/ExceptionExample.java
@@ -4,12 +4,36 @@ public class ExceptionExample {
 
         //int c = divide(10,0);
 
-        int[] num = {10, 100, 200, 300};
-        int[] demo = {10, 100, 0, 300};
+        try {
 
-        for(int i =0 ; i< 4; i++){
-            System.out.println(divide(num[i], demo[i])) ;
+
+            String str = null;
+
+            System.out.println(str.length());
+
+
+            int[] num = {10, 100, 200, 300};
+            int[] demo = {10, 100, 0, 300};
+
+            for (int i = 0; i < 4; i++) {
+                System.out.println(divide(num[i], demo[i]));
+            }
+
+
+
+
+        }catch (NullPointerException  e){
+            //e.printStackTrace();
+            System.out.println("In the Exception");
+        }catch (ArithmeticException e){
+            //e.printStackTrace();
+            System.out.println("In the Exception");
+        }catch (RuntimeException e1){
+            System.out.println("In the e1 Exception");
+        }catch (Exception e1){
+            System.out.println("In the e1 Exception");
         }
+
 
         //System.out.println(c);
 
@@ -18,13 +42,13 @@ public class ExceptionExample {
     public static int divide(int a, int b){
         int d = 0;
 
-        try{
+        //try{
            d = a/b;
-        }catch (Exception e){
-            e.printStackTrace();
+        /*}catch (Exception e){
+        //    e.printStackTrace();
 
             System.out.println("In the Exception");
-        }
+        }*/
 
         return d ;
     }
