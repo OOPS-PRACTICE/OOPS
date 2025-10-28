870ae4ec094667c1721a5f5ad1b9d04cf7061959
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2025-10-28 08:57:36 +0530

    Create ErrorException.java

---
+++ exception/ErrorException.java
@@ -0,0 +1,12 @@
+public class ErrorException {
+
+    public static void main(String[] args) {
+
+        recursive();
+    }
+
+
+    public static void recursive(){
+        recursive();
+    }
+}
